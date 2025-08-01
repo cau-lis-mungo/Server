8be6f89c87dbb697bdc61e0e49e8f08cbb76c9a4
usernamehs <hsforwork123@gmail.com>
2025-08-01 17:18:22 +0900

    Feat: 도서 검색

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import BookLikedView
+from django.urls import path, include
+from .views import BookLikedView, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', BookViewSet, basename='book')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('<int:book_id>/like/', BookLikedView.as_view(), name='book-liked'),
 ]
