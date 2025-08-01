a8cef160f172e7cb28cfc707a215c3beec3040ee
usernamehs <hsforwork123@gmail.com>
2025-08-01 17:59:51 +0900

    Feat: admin 추가

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import BookLikedView, BookViewSet
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter # ViewSet을 자동으로 URL에 연결
 
 router = DefaultRouter()
 router.register(r'', BookViewSet, basename='book')
