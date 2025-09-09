2a4a14bde98d8dae4d194e79c8ac1956bf94b541
usernamehs <hsforwork123@gmail.com>
2025-09-09 19:27:59 +0900

    Fix: 검색어 최소 글자수 제한 삭제

--- books/filters.py
+++ books/filters.py
@@ -2,7 +2,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework.exceptions import ValidationError
 
 class MinLengthSearchFilter(SearchFilter):
-    min_length = 2 # 최소 글자 수
+    # min_length = 2 # 최소 글자 수
 
     def filter_queryset(self, request, queryset, view):
         # search 파라미터가 아예 없는 경우
@@ -17,10 +17,10 @@ class MinLengthSearchFilter(SearchFilter):
             return queryset
 
         # search 파라미터가 있고, 2글자 미만
-        if len(term) < self.min_length:
-            raise ValidationError({
-                self.search_param: f"검색어는 최소 {self.min_length}글자 이상 입력해야 합니다."
-            })
+        # if len(term) < self.min_length:
+        #     raise ValidationError({
+        #         self.search_param: f"검색어는 최소 {self.min_length}글자 이상 입력해야 합니다."
+        #     })
 
         # 2글자 이상
         return super().filter_queryset(request, queryset, view)
